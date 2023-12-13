cabbba22580993a643d8d3aa5b13aa7f6d0ad260
Emerson <emerson.art.studios@gmail.com>
2023-12-13 13:51:34 -0800

    added production server.js and database.js

---
+++ config/env/production/database.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  connection: {
+    connectionString: env("DATABASE_URL"),
+    host: env("DATABASE_HOST"),
+    port: env.int("DATABASE_PORT", 25060),
+    database: env("DATABASE_NAME"),
+    user: env("DATABASE_USERNAME"),
+    password: env("DATABASE_PASSWORD"),
+    ssl: env.bool("DATABASE_SSL", false) && {
+      key: env("DATABASE_SSL_KEY", undefined),
+      cert: env("DATABASE_SSL_CERT", undefined),
+      ca: env("DATABASE_SSL_CA", undefined),
+      capath: env("DATABASE_SSL_CAPATH", undefined),
+      cipher: env("DATABASE_SSL_CIPHER", undefined),
+      rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
+    },
+  },
+  debug: false,
+});
