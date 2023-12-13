c1edbcc2b91f9047747f03a7f9cefec836c87a92
Emerson <emerson.art.studios@gmail.com>
2023-12-12 16:56:20 -0800

    initial commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,78 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ArtworkAvailability extends Schema.Component {
+  collectionName: 'components_artwork_availabilities';
+  info: {
+    displayName: 'Availability';
+  };
+  attributes: {
+    isAvailable: Attribute.Boolean;
+    availability: Attribute.Enumeration<
+      ['Available', 'Unavailable', 'Sold', 'On Loan', 'On Consignment']
+    > &
+      Attribute.DefaultTo<'Available'>;
+  };
+}
+
+export interface ArtworkDimensions extends Schema.Component {
+  collectionName: 'components_artwork_dimensions';
+  info: {
+    displayName: 'Dimensions';
+  };
+  attributes: {
+    height: Attribute.Decimal;
+    width: Attribute.Decimal;
+    depth: Attribute.Decimal;
+    dimensions: Attribute.String;
+  };
+}
+
+export interface ArtworkPrice extends Schema.Component {
+  collectionName: 'components_artwork_prices';
+  info: {
+    displayName: 'Price';
+  };
+  attributes: {
+    price: Attribute.Decimal;
+    formattedPrice: Attribute.String;
+  };
+}
+
+export interface LocationAddress extends Schema.Component {
+  collectionName: 'components_location_addresses';
+  info: {
+    displayName: 'Address';
+  };
+  attributes: {
+    adressLineOne: Attribute.String;
+    addressLineTwo: Attribute.String;
+    City: Attribute.String;
+    state: Attribute.String;
+    zip: Attribute.String;
+    label: Attribute.String;
+  };
+}
+
+export interface LocationLocation extends Schema.Component {
+  collectionName: 'components_location_locations';
+  info: {
+    displayName: 'Location';
+  };
+  attributes: {
+    location: Attribute.String;
+    locationDetail: Attribute.String;
+    dateMoved: Attribute.Date;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'artwork.availability': ArtworkAvailability;
+      'artwork.dimensions': ArtworkDimensions;
+      'artwork.price': ArtworkPrice;
+      'location.address': LocationAddress;
+      'location.location': LocationLocation;
+    }
+  }
+}
